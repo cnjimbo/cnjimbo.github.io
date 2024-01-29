0ceb997b4f8871420bbf351aac381da72280fedf
cnjimbo <happycoder@foxmail.com>
2024-01-29 18:08:06 +0000

    Deploying to pages from @ cnjimbo/cnjimbo.github.io@47afee27f0d8e852fd8247a8de5d478c3265484a 🚀

---
+++ assets/app.tf31BkmN.js
@@ -0,0 +1,7 @@
+import{j as o,aP as p,aQ as u,aR as l,aS as c,aT as f,aU as d,aV as m,aW as h,aX as A,aY as g,Y as P,d as v,u as R,l as w,z as y,aZ as C,a_ as _,a$ as E,a7 as T}from"./chunks/framework.vTT8XiEL.js";import{R as b}from"./chunks/theme.kvStJS6x.js";function i(e){if(e.extends){const a=i(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const s=i(b),D=v({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=R();return w(()=>{y(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&C(),_(),E(),s.setup&&s.setup(),()=>T(s.Layout)}});async function S(){const e=L(),a=j();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),s.enhanceApp&&await s.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function j(){return h(D)}function L(){let e=o,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=P(()=>import(n),__vite__mapDeps([]))),o&&(e=!1),r},s.NotFound)}o&&S().then(({app:e,router:a,data:t})=>{a.go().then(()=>{p(a.route,t.site),e.mount("#app")})});export{S as createApp};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ assets/bigWeb_js_eventloop.md.hC9OSiMg.lean.js
@@ -0,0 +1,5 @@
+import{_ as t,E as k,c as p,m as s,J as n,w as l,V as i,o as e,a as h}from"./chunks/framework.vTT8XiEL.js";const _=JSON.parse('{"title":"EventLoop","description":"","frontmatter":{"sidebar":{"title":"Event Loop","step":12},"isTimeLine":true,"title":"EventLoop","date":"2020-04-14T00:00:00.000Z","tags":["大前端","javascript"],"categories":["大前端"]},"headers":[],"relativePath":"bigWeb/js/eventloop.md","filePath":"bigWeb/js/eventloop.md","lastUpdated":1671802927000}'),E={name:"bigWeb/js/eventloop.md"},r=i("",47),d=s("p",null," 输出结果",-1),g=s("pre",null,[s("code",null,`  
+  // 1 4 7 5 2 3 6
+  `)],-1),o=i("",2),y=s("p",null," 输出结果",-1),c=s("pre",null,[s("code",null,`  
+  // 1 4 10 5 6 7 2 3 9 8
+  `)],-1),F=i("",37);function C(u,m,B,b,v,A){const a=k("font");return e(),p("div",null,[r,s("details",null,[s("summary",null,[s("mark",null,[n(a,{color:"darkred"},{default:l(()=>[h("点击查看答案")]),_:1})])]),d,g]),o,s("details",null,[s("summary",null,[s("mark",null,[n(a,{color:"darkred"},{default:l(()=>[h("点击查看答案")]),_:1})])]),y,c]),F])}const T=t(E,[["render",C]]);export{_ as __pageData,T as default};
