f49e7fc3d183d2c47b7d7a113837257a8a5ae5ac
Jimbo Tang <happycoder@foxmail.com>
2024-02-06 18:49:17 +0800

    feat: pack the vue of the theme

--- packages/theme/tsup.config.ts
+++ packages/theme/tsup.config.ts
@@ -1,10 +1,28 @@
-import path from 'path'
+// import path from 'path'
 import { defineConfig } from 'tsup'
 
+import { sassPlugin } from 'esbuild-sass-plugin'
+import pluginVue from 'esbuild-plugin-vue-next'
+
 export default defineConfig({
-  entry: ['src/node.ts'],
-  outDir: path.resolve(__dirname, './'),
+  entry: ['src/node.ts', 'src/index.ts'],
+  // outDir: path.resolve(__dirname, './'),
   dts: true,
-  external: ['vitepress'],
   noExternal: ['vitepress-plugin-tabs'],
+  external: ['vitepress', 'vue'],
+  format: ['cjs', 'esm'],
+  shims: true,
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  minify: true,
+  treeshake: true,
+  bundle: true,
+  target: 'esnext',
+  esbuildPlugins: [
+    pluginVue(),
+    sassPlugin({
+      filter: /.(s[ac]ss|css)$/,
+    }),
+  ],
 })
