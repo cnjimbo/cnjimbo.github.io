8e4d11065ca7fafd804c9dbbad7d9ae6883c9565
dmsrs <happycoder@foxmail.com>
2024-05-28 00:06:30 +0800

    A

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,89 +1,56 @@
-import globals from "globals"
-// 预定义配置
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginVue from "eslint-plugin-vue"
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
 
-// import babelParser from "@typescript-eslint/parser";
-import commpnParser from "vue-eslint-parser"
-import prettier from "eslint-plugin-prettier"
-
-// "@babel/eslint-parser";
-
-// import customConfig from "./esconfig/custom_config.js";
-
-export default [
-  // languageOptions：配置如何检查 js 代码
-  {
-
-    // 1.1 处理 与 JavaScript 相关的配置项
-    // - ecmaVersion
-    // - sourceType
-    // - globals
-    // - parser
-    // - parserOptions
-    // files: ["**/*.ts", "**/*.vue"],
-    // ignores: ["**/*.config.js"],
-    ignores: [
-      "**/*.config.js",
-      "**/*.config.mjs",
-      "dist/**",
-      "node_modules/**",
-      "!**/eslint.config.js",
+// TODO DDL 迭代
+export default antfu({
+}, {
+  ignores: [
+    'packages/theme/*.js',
+  ],
+}, {
+  rules: {
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/extensions': 'off',
+    'quotes': [
+      'warn',
+      'single',
     ],
-    languageOptions: {
-      // 1.11 定义可用的全局变量
-      globals: globals.browser,
-      // 1.12 扩展
-      // ecmaVersion: "latest",
-      // sourceType: "module",
-      parser: commpnParser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        parser: "@typescript-eslint/parser",
-        jsxPragma: "React",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
+    'semi': [
+      'warn',
+      'never',
+    ],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-restricted-syntax': 'off',
+    'no-bitwise': 'off',
+    'camelcase': 'off',
+    'no-case-declarations': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'no-undef': 'off',
+    'no-shadow': 'off',
+    'max-len': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-use-before-define': 'off',
+    'no-continue': 'off',
+    'no-extend-native': 'warn',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'non-null-expression': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'consistent-return': 'off',
+    'no-console': 'off',
+    'ts/comma-dangle': 'off',
+    'curly': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+    'ts/no-var-requires': 'off',
+    'ts/no-require-imports': 'off',
+    'ts/ban-ts-comment': 'off',
+    'vue/prefer-import-from-vue': 'off',
+    'jsdoc/no-types': 'off',
+    'no-template-curly-in-string': 'off',
   },
-  // 原来的extends替换为如下模式
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {
-    globals: {
-      process: "readonly",
-    },
-    plugins: {
-      prettier,
-    },
-    rules: {
-      // 开启这条规则后，会将prettier的校验规则传递给eslint，这样eslint就可以按照prettier的方式来进行代码格式的校验
-      "prettier/prettier": "error",
-      // eslint（https://eslint.bootcss.com/docs/rules/）
-      "no-var": "error", // 要求使用 let 或 const 而不是 var
-      "no-multiple-empty-lines": ["warn", { max: 1 }], // 不允许多个空行
-      "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-      "no-unexpected-multiline": "error", // 禁止空余的多行
-      "no-useless-escape": "off", // 禁止不必要的转义字符
-
-      // typeScript (https://typescript-eslint.io/rules)
-      "@typescript-eslint/no-unused-vars": "error", // 禁止定义未使用的变量
-      "@typescript-eslint/prefer-ts-expect-error": "error", // 禁止使用 @ts-ignore
-      "@typescript-eslint/no-explicit-any": "off", // 禁止使用 any 类型
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-namespace": "off", // 禁止使用自定义 TypeScript 模块和命名空间。
-      "@typescript-eslint/semi": "off",
-
-      // eslint-plugin-vue (https://eslint.vuejs.org/rules/)
-      "vue/multi-word-component-names": "off", // 要求组件名称始终为 “-” 链接的单词
-      "vue/script-setup-uses-vars": "error", // 防止<script setup>使用的变量<template>被标记为未使用
-      "vue/no-mutating-props": "off", // 不允许组件 prop的改变
-      "vue/attribute-hyphenation": "off", // 对模板中的自定义组件强制执行属性命名样式
-    },
-  },
-]
+})
