ee107abc464c3b461a73ef6058a3fe7c475af7b2
Jimbo <happycoder@foxmail.com>
2024-06-06 14:45:58 +0800

    build(blog): :construction: 更新oml2d js到unpkg.com

--- packages/blogpress/.vitepress/config.ts
+++ packages/blogpress/.vitepress/config.ts
@@ -71,13 +71,11 @@ const blogTheme = getThemeConfig({
     mobileDisplay: true,
     models: [
       {
-        path: 'https://sugarat.s3.bitiful.net/npm/oml2d-model/models/Senko_Normals/senko.model3.json',
+        path: 'https://model.oml2d.com/Senko_Normals/senko.model3.json',
       },
     ],
     libraryUrls: {
-      complete: 'https://sugarat.s3.bitiful.net/npm/oh-my-live2d/latest/lib/complete.js',
-      cubism2: 'https://sugarat.s3.bitiful.net/npm/oh-my-live2d/latest/lib/cubism2.js',
-      cubism5: 'https://sugarat.s3.bitiful.net/npm/oh-my-live2d/latest/lib/cubism5.js',
+      complete: 'https://unpkg.com/oh-my-live2d@latest',
     },
   },
   popover: {
