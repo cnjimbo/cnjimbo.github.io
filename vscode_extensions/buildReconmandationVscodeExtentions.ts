5a60b2375bcb384c78f10c55ca3b25068108b2a1
Jimbo <happycoder@foxmail.com>
2024-06-13 12:19:22 +0800

    chore(scaffold): :zap: workspace 修改推荐的vscode 插件列表

--- vscode_extensions/buildReconmandationVscodeExtentions.ts
+++ vscode_extensions/buildReconmandationVscodeExtentions.ts
@@ -1,4 +1,3 @@
-import data from './tswindows.json' assert{type: 'json'}
 // 使用code code runner 运行此脚本，生成推荐插件id列表。
 // code runner需要 pnpm add -D ts-node
 // 并且  需要设置code runner执行环境为
@@ -10,10 +9,18 @@ import data from './tswindows.json' assert{type: 'json'}
   },
 
   */
+
+import data from './tswindows.json' assert{type: 'json'}
+
+// const workspace_path = './../cnjimbo.github.io.code-workspace'
+const extensions = JSON.parse(data.extensions)
+
 const ids = []
-for (const m of data.extensions) {
+for (const m of extensions) {
   if (!m.disabled) {
     ids.push(m.identifier.id)
   }
 }
+console.log('')
+console.log('')
 console.log(`["${ids.join('","')}"]`)
