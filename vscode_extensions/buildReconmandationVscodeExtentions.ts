de2d615c17c595efae758be307feab1a7ed58b73
Jimbo <happycoder@foxmail.com>
2024-06-19 06:26:46 +0000

    Invalid DateTime

--- vscode_extensions/buildReconmandationVscodeExtentions.ts
+++ vscode_extensions/buildReconmandationVscodeExtentions.ts
@@ -11,8 +11,36 @@
   */
 // 最后将输出内容复制到code-workspace的对应位置
 
-import data from './tswindows.json' assert{type: 'json'}
+import * as fs from 'fs';
 
+import data from './tswindows.json' assert{type: 'json'}
+// 定义一个异步函数来读取文件并解析为JSON
+async function readFileToJson(filePath: string): Promise<any> {
+  try {
+    // 读取文件内容
+    const fileContent = await fs.promises.readFile(filePath, 'utf8');
+
+    // 将文件内容解析为JSON对象
+    const jsonObject = JSON.parse(fileContent);
+
+    return jsonObject;
+  } catch (error) {
+    // 处理读取文件或解析JSON时的错误
+    console.error(`Error reading or parsing JSON from file: ${error}`);
+    throw error;
+  }
+}
+
+// 使用示例
+(async () => {
+  try {
+    const filePath = './data.json'; // 假设data.json是你要读取的文件路径
+    const jsonData = await readFileToJson(filePath);
+    console.log(jsonData);
+  } catch (error) {
+    // 这里可以进一步处理错误
+  }
+})();
 // const workspace_path = './../cnjimbo.github.io.code-workspace'
 const extensions = JSON.parse(data.extensions)
 
