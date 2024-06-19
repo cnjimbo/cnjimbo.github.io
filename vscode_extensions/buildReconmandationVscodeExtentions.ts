f3ed6010337ebbfaec77e395db0ab65922113b57
dmsrs <happycoder@foxmail.com>
2024-06-19 22:41:29 +0800

    refactor(vscode_extensions): optimize build script and dependency management
    Optimize the VSCode extensions build script by removing unnecessary packages and
    cleaning up the import statements. Simplify the 'code-runner.executorMap' configuration
    for TypeScript and JavaScript execution, ensuring compatibility across different
    environments. Adjust the workspace settings to streamline the development experience
    by disabling certain formatters and enabling auto-saving. Update package dependencies
    to ensure only necessary packages are included in the project.
    
    BREAKING CHANGE: Removal of some formatters and the default TypeScript TSDK configuration
    may impact the development workflow for users accustomed to these settings.

--- vscode_extensions/buildReconmandationVscodeExtentions.ts
+++ vscode_extensions/buildReconmandationVscodeExtentions.ts
@@ -2,39 +2,26 @@
 // code runner需要 pnpm add -D ts-node
 // 并且  需要设置code runner执行环境为
 /*
-"code-runner.executorMap": {
-    "typescript": "node --es-module-specifier-resolution=node --loader ts-node/esm $fullFileName",
-    "ts": "node --es-module-specifier-resolution=node --loader ts-node/esm $fullFileName",
-    "javascript": "node  --es-module-specifier-resolution=node  $fullFileName",
-  },
+    "code-runner.executorMap": {
+      "typescript": "cd $dir && node --es-module-specifier-resolution=node --loader ts-node/esm $fullFileName",
+      "javascript": "cd $dir && node  --es-module-specifier-resolution=node  $fullFileName"
+    },
 
   */
 // 最后将输出内容复制到code-workspace的对应位置
 import * as fs from 'node:fs'
-import { parse, ParseContext } from "bao-json"
+import JSON5 from 'json5'
 
-function parseJsonWithComments(jsonString) {
-
-  const parseContext: ParseContext = parse(jsonString)
-
-  try {
-    return parseContext
-  }
-  catch (e) {
-    console.error("解析错误:", e)
-    return null
-  }
+function parseJsonWithComments(jsonString: string) {
+  return JSON5.parse(jsonString)
 }
 
 async function readFileToJson(filePath: string): Promise<any> {
   const fileContent = await fs.promises.readFile(filePath, 'utf8')
-
-  // 将文件内容解析为JSON对象
   const jsonObject = parseJsonWithComments(fileContent)
-
   return jsonObject
 }
-// 定义一个异步函数来写入文件
+
 async function writeJsonToFile(filePath: string, content: string): Promise<void> {
   await fs.promises.writeFile(filePath, content, 'utf8')
   console.log(`JSON data has been successfully written to ${filePath}`)
@@ -50,6 +37,8 @@ async function findInstalledExtensions(data): Promise<string[]> {
   }
   return ids
 }
+
+console.log('-----------------------------', 'start', '-----------------------------')
 const codeProfile = './tswindows.code-profile'
 const codeWorkspace = './../cnjimbo.github.io.code-workspace'
 readFileToJson(codeProfile)
@@ -61,7 +50,6 @@ readFileToJson(codeProfile)
     return { ids, target }
   })
   .then(({ ids, target }) => {
-    console.log("ids", ids)
     target.extensions.recommendations = ids
-    // writeJsonToFile(codeWorkspace, JSON.stringify(target))
+    writeJsonToFile(codeWorkspace, JSON.stringify(target, null, "\t"))
   })
