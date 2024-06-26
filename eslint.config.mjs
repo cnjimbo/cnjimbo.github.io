e3a68ae492c902d397dd899781b52561996bead6
Jimbo <happycoder@foxmail.com>
2024-06-26 17:35:39 +0800

    style(eslint): update config and fix rule inconsistencies
    - Enforce single quotes consistency in eslint config.
    - Ensure correct ignore patterns for node_modules and .history directories.
    - Adjust file inclusion patterns for TypeScript and Vue files.
    - Resolve rule inconsistencies and remove deprecated rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,108 +1,93 @@
 // eslint.config.js
-import antfu from "@antfu/eslint-config"
-// import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import tseslint from 'typescript-eslint'
-import vuePlugin from 'eslint-plugin-vue'
-import vueParser from 'vue-eslint-parser'
+import antfu from '@antfu/eslint-config'
+// import tseslint from 'typescript-eslint'
+// import vuePlugin from 'eslint-plugin-vue'
+// import vueParser from 'vue-eslint-parser'
 
 export default antfu(
   {
     ignores: [
       // "packages/theme/dist/*.js",
-      "**/node_modules",
+      '**/node_modules',
       // '**/*.md',
       // "**/*.yml",
-      "**/.history/**",
+      '**/.history/**',
       '**/dist/',
       '**/cache/',
     ],
   },
-
-  {
-    files: ['packages/**/*.{ts,tsx}'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-this-alias': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-var-requires': 'warn',
-      'no-shadow': 'off',
-      '@typescript-eslint/no-shadow': 'error',
-      "@stylistic/indent": ["off", 2],
-      "@stylistic/indent-binary-ops": ["off", 2],
-      "@stylistic/*": 0,
-      "style/member-delimiter-style": 0,
-      "semi": 0,
-      "style/semi": 0,
-      "astro/semi": 0,
-
-    }
-  },
-
-  {
-    files: ['packages/vue/**/*.{ts,vue}'],
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: tseslint.parser,
-        extraFileExtensions: ['.vue'],
-        project: true
-      }
-    },
-    plugins: {
-      vuePlugin
-    },
-    rules: {
-      ...vuePlugin.configs['flat/recommended'].rules
-    }
-  },
+  // {
+  //   files: ['packages/**/*.{vue}'],
+  //   languageOptions: {
+  //     parser: vueParser,
+  //     parserOptions: {
+  //       parser: tseslint.parser,
+  //       extraFileExtensions: ['.vue'],
+  //       project: true
+  //     }
+  //   },
+  //   plugins: {
+  //     vuePlugin
+  //   },
+  //   rules: {
+  //     ...vuePlugin.configs['flat/recommended'].rules
+  //   }
+  // },
 
   // eslintPluginPrettierRecommended,
   {
     rules: {
-      "ts/no-shadow": 0,
-      "style/comma-dangle": "off",
-      "jsonc/sort-keys": "off",
-      "@typescript-eslint/ban-types": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "import/extensions": "off",
-
-      "semi": 0,
-      "import/no-unresolved": "off",
-      "import/prefer-default-export": "off",
-      "no-param-reassign": "off",
-      "import/no-extraneous-dependencies": "off",
-      "no-restricted-syntax": "off",
-      "no-bitwise": "off",
-      "camelcase": "off",
-      "no-case-declarations": "off",
-      "@typescript-eslint/no-namespace": "off",
-      "no-undef": "off",
-      "no-shadow": "off",
-      "max-len": "off",
-      "@typescript-eslint/no-var-requires": "off",
-      "no-use-before-define": "off",
-      "no-continue": "off",
-      "no-extend-native": "warn",
-      "no-underscore-dangle": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "non-null-expression": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "consistent-return": "off",
-      "no-console": "off",
-      "ts/comma-dangle": "off",
-      "curly": "off",
-      "unicorn/prefer-node-protocol": "off",
-      "ts/no-var-requires": "off",
-      "ts/no-require-imports": "off",
-      "ts/ban-ts-comment": "off",
-      "vue/prefer-import-from-vue": "off",
-      "jsdoc/no-types": "off",
-      "no-template-curly-in-string": "off",
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'import/extensions': 'off',
+      'quotes': [
+        'warn',
+        'single',
+      ],
+      'semi': [
+        'warn',
+        'never',
+      ],
+      'import/no-unresolved': 'off',
+      'import/prefer-default-export': 'off',
+      'no-param-reassign': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'no-restricted-syntax': 'off',
+      'no-bitwise': 'off',
+      'camelcase': 'off',
+      'no-case-declarations': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      'no-undef': 'off',
+      'no-shadow': 'off',
+      'max-len': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-use-before-define': 'off',
+      'no-continue': 'off',
+      'no-extend-native': 'warn',
+      'no-underscore-dangle': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'non-null-expression': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'consistent-return': 'off',
+      'no-console': 'off',
+      'ts/comma-dangle': 'off',
+      'curly': 'off',
+      'unicorn/prefer-node-protocol': 'off',
+      'ts/no-var-requires': 'off',
+      'ts/no-require-imports': 'off',
+      'ts/ban-ts-comment': 'off',
+      'vue/prefer-import-from-vue': 'off',
+      'jsdoc/no-types': 'off',
+      'no-template-curly-in-string': 'off',
       // ----new test -----
-      "no-var": "warn",
-      "style/quotes": ['off', 'single'],
-      "quotes": "off"
+      'no-var': 'warn',
+      'style/quotes': ['off', 'single'],
+      // "quotes": "off",
+      'ts/no-unused-vars': 'off',
+      'ts/no-shadow': 0,
+      'style/comma-dangle': 'off',
+      'jsonc/sort-keys': 'off',
     },
   },
 )
