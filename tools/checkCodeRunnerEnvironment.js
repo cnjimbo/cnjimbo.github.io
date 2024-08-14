f05561f7e55a253170a3adf1f106935f5882f86b
dmsrs <happycoder@foxmail.com>
2024-08-15 00:21:25 +0800

    "优化code-runner环境检查与设置合并逻辑"

--- tools/checkCodeRunnerEnvironment.js
+++ tools/checkCodeRunnerEnvironment.js
@@ -8,126 +8,164 @@ import _ from 'lodash'
 function parseJsonWithComments(jsonString) {
   return JSON5.parse(jsonString)
 }
-
+/**
+ *
+ * @param {string} filePath new settings
+ * @returns {object} json data
+ */
 async function readFileToJson(filePath) {
-  if (!fs.existsSync(filePath))
-    return {}
-  console.log('配置文件：', path.resolve(process.cwd(), filePath))
-  const fileContent = fs.readFileSync(path.resolve(filePath), 'utf8')
-  // console.log('配置内容：', fileContent)
-  const jsonObject = parseJsonWithComments(fileContent)
-  return { filePath, jsonObject }
-}
-
-async function writeJsonToFile(filePath, jsonObject) {
-  console.log(filePath)
-  const content = JSON.stringify(jsonObject, null, '  ')
-  await fs.promises.writeFile(filePath, content, 'utf8')
-  console.log(`JSON data has been successfully written to ${filePath}`)
-  return { filePath, jsonObject }
-}
-async function _findInstalledExtensions(data) {
-  const extensions = parseJsonWithComments(data.extensions)
-  const ids = []
-  for (let i = 0, n = extensions.length; i < n; i++) {
-    const m = extensions[i]
-    if (!m.disabled) {
-      ids.push(m.identifier.id)
-    }
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`file not exist: ${filePath}`)
   }
-  return ids
-}
-
-async function createNewSettings(filePath, newSettings) {
-  return fs.ensureFile(filePath)
-    .then(async value => await writeJsonToFile(value, newSettings))
-}
-async function mergeToCurrentSettings(filePath, newSettings) {
-  return readFileToJson(extensionWorkspace)
-    .then(async (currSettings) => {
-      console.log('check', currSettings)
-      return tryMerge(currSettings, newSettings)
-    })
-    .then(async ({ needMerge, filePath, jsonObject }) => {
-      if (needMerge === true) {
-        const o = await writeJsonToFile(filePath, jsonObject)
-        console.log('--------------------------  overrided file:', filePath, '-----------------------------  ')
-        return o
-      }
-      console.log('--------------------------  Keep file:', filePath, '-----------------------------  ')
-      return { needMerge, filePath, jsonObject }
-    })
+  const fileContent = fs.readFileSync(filePath, 'utf8')
+  if (!fileContent || fileContent.length === 0)
+    throw new Error(`do not read any json string from file: ${filePath}`)
+  return parseJsonWithComments(fileContent)
 }
 /**
  *
- * @param {{filepath:string,jsonObject:object}} currSettings current settings
- * @param {{filepath:string,jsonObject:object}} newSettings new settings
- * @returns {{needMerge:boolean,jsonObject:object}} merged ojbec
+ * @param {string} filePath
+ * @param {object} jsonObject
  */
-async function tryMerge(currSettings, newSettings) {
-  const keys = Object.keys(currSettings)
-  let needMerge = false
-  for (const key in Object.keys(newSettings)) {
-    if (!keys.includes(key)) {
-      needMerge = true
-      break
+async function writeJsonToFile(filePath, jsonObject) {
+  const content = JSON.stringify(jsonObject, null, '  ')
+  return fs.writeFile(filePath, content, 'utf8')
+    .then(() => {
+      console.log(`JSON data has been successfully written to ${filePath}`)
+      return true
+    })
+    .catch((err) => {
+      console.log(`error on writting to ${filePath}`, err)
+      return false
+    })
+}
+
+function deepEqual(obj1, obj2) {
+  const keys1 = Object.keys(obj1)
+  const keys2 = Object.keys(obj2)
+
+  if (keys1.length !== keys2.length)
+    return false
+
+  for (const key of keys1) {
+    if (!keys2.includes(key))
+      return false
+
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      if (!deepEqual(obj1[key], obj2[key]))
+        return false
+    }
+    else {
+      if (obj1[key] !== obj2[key])
+        return false
     }
   }
-  let obj = { needMerge, ...currSettings }
-  if (needMerge) {
-    const mergedSettings = _.merge({}, newSettings, currSettings)
-    obj = { needMerge, jsonObject: mergedSettings }
+
+  // 所有键值对都相等
+  return true
+}
+/**
+ *
+ * @param {{filepath:string,:object}} preferSettings current settings
+ * @param {{filepath:string,:object}} newSettings new settings
+ * @returns {{needRewrite:boolean,data:object}} merged ojbec
+ */
+async function tryMerge(preferSettings, newSettings) {
+  const needRewrite = !deepEqual(preferSettings, newSettings)
+  const obj = { needRewrite, data: preferSettings }
+  if (needRewrite) {
+    obj.data = _.merge({}, newSettings, preferSettings)
   }
   return obj
 }
 
 console.log('-----------------------------', 'start', '-----------------------------')
-const codeWorkspace = '**/*.code-workspace'
-const extensionWorkspace = path.resolve(process.cwd(), '.vscode/settings.json')
+const defaultSettings = {
+  'code-runner.executorMap': {
+    typescript: 'cd $dir && npx tsx $fullFileName'
+  },
+  'code-runner.executorMapByFileExtension': {
+    '.ts': 'cd $dir && npx tsx $fullFileName'
+  }
+}
 
-function main() {
-  const defaultSettings = {
-    'code-runner.executorMap': {
-      typescript: 'cd $dir && npx tsx $fullFileName'
-    },
-    'code-runner.executorMapByFileExtension': {
-      '.ts': 'cd $dir && npx tsx $fullFileName'
+async function checkCodeWorkspaceFilePath(codeWorkspaceFilePath) {
+  return await glob(codeWorkspaceFilePath, { cwd: process.cwd(), absolute: true })
+    .then((files) => {
+      if (files && files.length > 0) {
+        if (files.length > 1) {
+          console.log('only support one file with .code-workspace in the current project. selected file:', files[0])
+        }
+        return files[0]
+      }
+      console.log(`can't find any code-workspace file with parttern: ${codeWorkspaceFilePath}`)
+      return undefined
+    })
+}
+
+function objectKeysIncludes(subObj, parentObj) {
+  if (parentObj == null)
+    throw new Error('parent object can\'t be undefined or null')
+  if (subObj == null)
+    return true
+  const sourceKeys = Object.keys(subObj)
+  const referenceKeys = Object.keys(parentObj)
+  // 检查 subObj 的所有键是否都在 parentObj 中
+  return sourceKeys.every(key => referenceKeys.includes(key))
+}
+
+async function main() {
+  const codeWorkspaceFilePath = './../*.code-workspace'
+  const vs_filepath = path.resolve(process.cwd(), '../.vscode/settings.json')
+
+  const cwfile = await checkCodeWorkspaceFilePath(codeWorkspaceFilePath)
+  const exist_codework_file = fs.existsSync(cwfile)
+  const exist_vs_setting_file = fs.existsSync(vs_filepath)
+
+  if (exist_codework_file && exist_vs_setting_file) {
+    const cw = await readFileToJson(cwfile)
+    const cw_setting = cw.settings
+    const vs_setting = await readFileToJson(vs_filepath)
+    const merged = tryMerge(cw_setting, vs_setting)
+    const config = ensureConfiged(merged.data, defaultSettings)
+    if (config.needRewrite || merged.needRewrite) {
+      cw.settings = config.data
+      vs = config.data
+      await writeJsonToFile(cwfile, cw)
+      await writeJsonToFile(vs_filepath, vs)
     }
   }
-  const handle = fs.existsSync(extensionWorkspace)
-    ? mergeToCurrentSettings(extensionWorkspace, defaultSettings)
-    : createNewSettings(extensionWorkspace, defaultSettings)
+  else if (exist_codework_file) {
+    const cw = await readFileToJson(cwfile)
+    const cw_setting = cw.settings
+    const config = ensureConfiged(cw_setting, defaultSettings)
+    if (config.needRewrite) {
+      cw.settings = config.data
+      await writeJsonToFile(cwfile, cw)
+    }
+  }
+  else if (exist_vs_setting_file) {
+    console.log('cccccc')
+    let vs = await readFileToJson(vs_filepath)
+    const config = ensureConfiged(vs, defaultSettings)
+    if (config.needRewrite) {
+      vs = config.data
+      await writeJsonToFile(vs_filepath, vs)
+    }
+  }
+  else {
+    await writeJsonToFile(vs_filepath, defaultSettings)
+  }
+}
 
-  handle
-    .then(({ needMerge, filePath, jsonObject }) => {
-      return { needMerge, filePath, jsonObject }
-    })
-    .then(result => glob(codeWorkspace, {
-      cwd: process.cwd(),
-      absolute: true
-    })
-      .then(async (files) => {
-        if (files && files.length > 0) {
-          const filePath = files[0]
-          if (fs.existsSync(filePath)) {
-            const codeprofileReadObject = await readFileToJson(filePath)
-            const currSettings = codeprofileReadObject.jsonObject.settings
-            if (currSettings) {
-              const merged = tryMerge(currSettings, result.jsonObject)
-              if (merged.needMerge) {
-                codeprofileReadObject.settings = merged.jsonObject
-                await writeJsonToFile(filePath, codeprofileReadObject)
-                console.log('--------------------------  overrided file:', filePath, '-----------------------------  ')
-              }
-              else {
-                console.log('--------------------------  Keep file:', filePath, '-----------------------------  ')
-              }
-            }
-          }
-        }
-      }))
-
-    .catch(err => console.error(err))
+function ensureConfiged(currentSettings, part_settings) {
+  const needRewrite = !objectKeysIncludes(part_settings, currentSettings)
+  if (needRewrite) {
+    return tryMerge(currentSettings, part_settings)
+  }
+  return { needRewrite: false, data: currentSettings }
 }
 
 main()
+  .catch(err => console.log('error:', err))
+  .finally(() => console.log('running end, exist'))
